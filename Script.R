9cba6c2a42af757e330bb790c61f54663650a67f
liber09 <113246195+liber09@users.noreply.github.com>
2022-11-18 03:59:35 +0100

    solved q4 and q1

--- Script.R
+++ Script.R
@@ -6,3 +6,12 @@ for (i in UKgas){
   if(counter %% n == 0)
     q4 <- c(q4,i)
 }
+
+q1 <- numeric(0)
+n <- 4
+counter <- -1
+for (i in UKgas){
+  counter = counter+1
+  if(counter %% n == 0)
+    q1 <- c(q1,i)
+}
